c82273b1ca3c35371642120aad9076a716f29c6d
ElvisCuro <elviscuro0@gmail.com>
2025-01-14 23:09:35 -0500

    create packages

--- src/main/java/com/challenge/literalura/application/LiteraluraApplication.java
+++ src/main/java/com/challenge/literalura/application/LiteraluraApplication.java
@@ -1,4 +1,4 @@
-package com.challenge.literalura;
+package com.challenge.literalura.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
